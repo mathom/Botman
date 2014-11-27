b275be3a2f4c3346366dcdb22a7adb2261c3d493
Matt Thompson <chameleonator@gmail.com>
2014-11-27 03:19:54 -0600

    Adding magic 8ball

--- bot.lua
+++ bot.lua
@@ -145,6 +145,17 @@ function commands.c_say(user, args)
     play_soundfile(out, 1.0, user)
 end
 
+commands.h_8ball='Ask the magic 8 Ball a question.'
+function commands.c_8ball(user, args)
+   local responses = {"As I see it, yes","It is certain","It is decidedly so","Most likely","Outlook good","Signs point to yes","Without a doubt","Yes","Yes definitely","You may rely on it","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"}
+   local answer = responses[math.random(#responses)]
+
+   if args[1] == 'voice' then
+       commands.c_say(user, {answer})
+   end
+   user.channel:send(answer)
+end
+
 commands.h_playlist='List files available to !play.'
 function commands.c_playlist(user, args)
     local ls = io.popen('ls -sh1 sounds')

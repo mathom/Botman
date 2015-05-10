3d5cbeaf24ee50d2e79967aa5d7643cd68388b37
Matt Thompson <chameleonator@gmail.com>
2015-05-09 19:03:27 -0500

    Drop video if input is a video file

--- convert.sh
+++ convert.sh
@@ -2,5 +2,5 @@
 
 input="$1"
 base=`basename ${input%.*}`
-avconv -i "$input" -ac 1 -ar 44100 -codec:a libvorbis $2 sounds/$base.ogg
+avconv -i "$input" -vn -ac 1 -ar 44100 -codec:a libvorbis $2 sounds/$base.ogg
 normalize-ogg sounds/$base.ogg
